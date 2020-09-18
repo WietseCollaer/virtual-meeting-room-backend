5a1108a2cf75ee01703a0265985973a122a3082a
WietseCollaer <wietse.collaer@live.com>
2020-09-18 10:03:47 +0200

    Initial commit

---
+++ src/main/java/com/cegeka/virtualmeetingrooms/VirtualmeetingroomsApplication.java
@@ -0,0 +1,13 @@
+package com.cegeka.virtualmeetingrooms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualmeetingroomsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VirtualmeetingroomsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cegeka/virtualmeetingrooms/VirtualmeetingroomsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cegeka.virtualmeetingrooms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtualmeetingroomsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
